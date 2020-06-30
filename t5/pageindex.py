1e46e515c44daa01c4f88b5e7b656ac70357cd11
Nicolás <martineznicoe@gmail.com>
2020-06-30 14:30:38 -0300

    Se agrega WaitExplicit a pageindex

--- t5/pageindex.py
+++ t5/pageindex.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class PageIndex():
     def __init__(self, my_driver):
@@ -7,5 +9,14 @@ class PageIndex():
         self.driver = my_driver
 
     def search(self, item):
-        self.driver.find_element(*self.query_top).send_keys(item)
-        self.driver.find_element(*self.query_button).click()
+        #Necesito un web element "search_box"
+        #para que aparezca tengo que esperar (tiene un tiempo máximo de 10 seg) una condición (until),
+        #que espere hasta que el elemento "query_top" exista.
+        search_box = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.query_top))
+        search_box.send_keys(item)
+
+        # Necesito un web element "search_button"
+        # para que aparezca tengo que esperar (tiene un tiempo máximo de 10 seg) una condición (until),
+        # que espere hasta que el elemento "query_button" sea clickeable.
+        search_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.query_button))
+        search_button.click()
